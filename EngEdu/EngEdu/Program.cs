4fa3498c6da95015dc34ed8cd73b5d1f571b2d5d
Aomnaherb <sosaom1@gmail.com>
2019-08-30 23:10:10 +0700

    2561 semester 2

--- EngEdu/EngEdu/Program.cs
+++ EngEdu/EngEdu/Program.cs
@@ -15,6 +15,7 @@ namespace EngEdu
             Console.WriteLine("Year 2560 semester 1, I start my programmer life.");
             Console.WriteLine("Year 2560 semester 2, I learn Solve math so fokking rock my Self.");
             Console.WriteLine("Year 2561 semester 1, I worry grade Lap is so bad and I know University kick me out.");
+            Console.WriteLine("Year 2561 semester 2, I choose to study subjects Calculus into class schedule.");
 
             Console.ReadKey();
 
